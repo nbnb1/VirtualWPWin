4e79d6bab8af2a4f72080bee027ba762621309d5
Ricardo Xie <709090316@qq.com>
2017-08-25 12:56:50 -0400

    menu improvement

--- wp-content/themes/startwordpress/header.php
+++ wp-content/themes/startwordpress/header.php
@@ -31,10 +31,7 @@
       <div class="container">
         <nav class="blog-nav">
           <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
+          <?php wp_list_pages( '&title_li=' ); ?>
         </nav>
       </div>
     </div>
@@ -42,6 +39,6 @@
     <div class="container">
 
       <div class="blog-header">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
-        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+        <h1 class="blog-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+        <p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
       </div>
